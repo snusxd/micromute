c8785b19303d403aad8e57e24b64c48026fb3786
snus xD <voroninkosta31@gmail.com>
2026-02-07 16:50:44 +0300

    Initial Commit

---
+++ micromute/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  micromute
+//
+//  Created by snus on 07.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ micromute/micromuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  micromuteApp.swift
+//  micromute
+//
+//  Created by snus on 07.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct micromuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
